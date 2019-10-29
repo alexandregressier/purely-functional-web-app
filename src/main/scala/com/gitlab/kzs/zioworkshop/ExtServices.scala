c781c6a51f8fddd879e1abaf7b87a9a4bd537f58
Alexandre Gressier <alexandre@gressier.dev>
2019-10-29 11:39:15 +0100

    Create the ExtServices class

---
+++ src/main/scala/com/gitlab/kzs/zioworkshop/ExtServices.scala
@@ -0,0 +1,23 @@
+package com.gitlab.kzs.zioworkshop
+
+import com.gitlab.kzs.zioworkshop.dao.{StockDAO, StockDAOLive}
+import doobie.util.transactor.Transactor
+import zio.Task
+import zio.clock.Clock
+import zio.interop.catz._
+
+trait ExtServices extends Clock {
+  val stockDAO: StockDAO
+}
+
+object ExtServicesLive extends ExtServices with Clock.Live {
+
+  val xa = Transactor.fromDriverManager[Task] (
+    "org.h2.Driver",
+    "jdbc:h2:mem:poc;INIT=RUNSCRIPT FROM 'src/main/resources/sql/create.sql'"
+    , "sa", ""
+  )
+
+  override val stockDAO: StockDAO = new StockDAOLive(xa)
+
+}
