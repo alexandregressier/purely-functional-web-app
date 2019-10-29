7b32cbb224b3e106e96c0056c5a5f9bb5b061f44
Alexandre Gressier <alexandre@gressier.dev>
2019-10-29 13:46:47 +0100

    Update the specification tests for Stock

--- src/test/scala/com/gitlab/kzs/zioworkshop/StockSpec.scala
+++ src/test/scala/com/gitlab/kzs/zioworkshop/StockSpec.scala
@@ -1,40 +1,43 @@
 package com.gitlab.kzs.zioworkshop
 
 import com.gitlab.kzs.zioworkshop.dao.StockDAO
-import com.gitlab.kzs.zioworkshop.model.{Stock, StockDBAccessError, StockNotFound}
+import com.gitlab.kzs.zioworkshop.model.{Stock, StockDBAccessError, StockError, StockNotFound}
 import org.http4s._
 import org.http4s.syntax.kleisli._
 import org.specs2.mutable.Specification
+import zio.clock.Clock
+import zio.internal.PlatformLive
 import zio.interop.catz._
-import zio.{DefaultRuntime, IO, Task}
+import zio.{IO, Runtime, Task}
 
 class StockSpec extends Specification {
 
-  val fakeStockDAO: StockDAO = new StockDAO {
+  object ExtServicesTest extends ExtServices with Clock.Live {
+    override val stockDAO: StockDAO = new StockDAO {
 
-    // You could also use a mocking framework here
-    override def currentStock(stockId: Int): Task[Stock] = {
-      stockId match {
-        case 1 => IO.succeed(Stock(1, 10))
-        case 2 => IO.succeed(Stock(2, 15))
-        case 3 => IO.succeed(Stock(3, 0))
-        case 99 => IO.fromEither(Left(StockDBAccessError(new Exception("BOOM!"))))
-        case _ => IO.fromEither(Left(StockNotFound))
-      }
+      override def currentStock(stockId: Int): IO[StockError, Stock] =
+      // You could also use a mocking framework here
+        stockId match {
+          case 1 => IO.succeed(Stock(1, 10))
+          case 2 => IO.succeed(Stock(2, 15))
+          case 3 => IO.succeed(Stock(3, 0))
+          case 99 => IO.fromEither(Left(StockDBAccessError(new Exception("BOOM!"))))
+          case _ => IO.fromEither(Left(StockNotFound))
+        }
+
+      override def updateStock(stockId: Int, increment: Int): IO[StockError, Stock] =
+        currentStock(stockId).map { stock => stock.copy(value = stock.value + increment) }
     }
-
-    override def updateStock(stockId: Int, updateValue: Int): Task[Stock] = ???
-
   }
 
-  val testRuntime = new DefaultRuntime {}
+  val testRuntime = Runtime(ExtServicesTest, PlatformLive.Default)
 
   "Stock HTTP Service" should {
     "return 200 and current stock" in {
-      val request = Request[Task](Method.GET, uri"""/stock/1""")
-      val stockResponse = testRuntime.unsafeRun(new HTTPService(fakeStockDAO).routes.orNotFound.run(request))
+      val request = Request[Task] (Method.GET, uri"""/stock/1""")
+      val stockResponse = testRuntime.unsafeRun(HTTPService.routes.orNotFound.run(request))
       stockResponse.status must beEqualTo(Status.Ok)
-      testRuntime.unsafeRun(stockResponse.as[String]) must beEqualTo("""{"id":1,"value":"10"}""")
+      testRuntime.unsafeRun(stockResponse.as[String]) must beEqualTo("""{"id":1,"value":10}""")
     }
 
 //    "return 200 and updated stock" in {
