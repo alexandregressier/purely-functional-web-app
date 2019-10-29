52f32e96dea0e94df4584f7b4244f932b692ea7f
Alexandre Gressier <alexandre@gressier.dev>
2019-10-29 09:24:20 +0100

    Clean up the Stock class

--- src/main/scala/com/gitlab/kzs/zioworkshop/model/Stock.scala
+++ src/main/scala/com/gitlab/kzs/zioworkshop/model/Stock.scala
@@ -1,6 +1,6 @@
 package com.gitlab.kzs.zioworkshop.model
 
-case class Stock(id:Int, value: Int)
+case class Stock(id: Int, value: Int)
 
 sealed abstract class StockError(cause: Throwable) extends Exception(cause)
 case object EmptyStock extends StockError(new Exception)
@@ -10,11 +10,10 @@ case class StockDBAccessError(cause: Throwable) extends StockError(cause)
 /**
   * Stock business logic
   */
-
 object Stock {
 
   /**
-   * Checks that stock is not empty
-   */
+    * Checks that stock is not empty.
+    */
   def validate(stock: Stock): Either[StockError, Stock] = ???
 }
