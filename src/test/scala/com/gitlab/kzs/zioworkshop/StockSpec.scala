a22c3beecb2f3c36971aa7a692ab09273908f3e7
Alexandre Gressier <alexandre@gressier.dev>
2019-11-01 13:06:00 +0100

    Update the ExtServicesTest class and fix the broken tests

--- src/test/scala/com/gitlab/kzs/zioworkshop/StockSpec.scala
+++ src/test/scala/com/gitlab/kzs/zioworkshop/StockSpec.scala
@@ -2,6 +2,7 @@ package com.gitlab.kzs.zioworkshop
 
 import com.gitlab.kzs.zioworkshop.dao.StockDAO
 import com.gitlab.kzs.zioworkshop.model.{Stock, StockDBAccessError, StockError, StockNotFound}
+import com.gitlab.kzs.zioworkshop.stream.FileStream
 import org.http4s._
 import org.http4s.syntax.kleisli._
 import org.specs2.mutable.Specification
@@ -9,58 +10,49 @@ import zio.clock.Clock
 import zio.internal.PlatformLive
 import zio.interop.catz._
 import zio.{IO, Runtime, Task}
+import fs2.Stream
 
 class StockSpec extends Specification {
 
   object ExtServicesTest extends ExtServices with Clock.Live {
 
     override val stockDAO: StockDAO = new StockDAO {
+
       override def findStock(stockId: Int): IO[StockError, Stock] =
-      // You could also use a mocking framework here
+      // A mocking framework could have been used as well
         stockId match {
           case 1 => IO.succeed(Stock(1, 10))
           case 2 => IO.succeed(Stock(2, 15))
           case 3 => IO.succeed(Stock(3, 0))
-          case 99 => IO.fromEither(Left(StockDBAccessError(new Exception("BOOM!"))))
+          case 99 => IO.fromEither(Left(StockDBAccessError(new Exception("Internal server error occurred"))))
           case _ => IO.fromEither(Left(StockNotFound))
         }
-
+      override def findAllStocks: fs2.Stream[Task, Stock] = Stream(
+        Stock(1, 10),
+        Stock(2, 15),
+        Stock(3, 0),
+      )
       override def updateStock(stockId: Int, increment: Int): IO[StockError, Stock] =
         findStock(stockId).map { stock => stock.copy(value = stock.value + increment) }
     }
+
+    override val fileStream: FileStream = new FileStream {
+      override def salesFromFile: Stream[STask, Stock] = Stream(
+        Stock(4, 20),
+        Stock(5, 25),
+      )
+    }
   }
 
-  val testRuntime = Runtime(ExtServicesTest, PlatformLive.Default)
+  val testRuntime: Runtime[ExtServicesTest.type] = Runtime(ExtServicesTest, PlatformLive.Default)
 
   "Stock HTTP Service" should {
-    "return 200 and current stock" in {
-      val request = Request[Task] (Method.GET, uri"""/stock/1""")
+
+    "respond 200 with the given stock" in {
+      val request = Request[STask] (Method.GET, uri"""/stocks/1""")
       val stockResponse = testRuntime.unsafeRun(HTTPService.routes.orNotFound.run(request))
       stockResponse.status must beEqualTo(Status.Ok)
       testRuntime.unsafeRun(stockResponse.as[String]) must beEqualTo("""{"id":1,"value":10}""")
     }
-
-//    "return 200 and updated stock" in {
-//      ???
-//    }
-  
-//    "return empty stock error" in {
-//      val request = Request[Task](Method.GET, uri"""/stock/3""")
-//      val stockResponse = testRuntime.unsafeRun(new HTTPService(fakeStockDAO).routes.orNotFound.run(request))
-//      stockResponse.status must beEqualTo(Status.Ok)
-//    }
-
-//    "return stock not found error" in {
-//      val request = Request[Task](Method.GET, uri"""/stock/4""")
-//      val stockResponse = testRuntime.unsafeRun(new HTTPService(fakeStockDAO).routes.orNotFound.run(request))
-//      stockResponse.status must beEqualTo(Status.Ok)
-//    }
-
-//    "return internal server error" in {
-//      val request = ???
-//      val stockResponse = ???
-//      stockResponse.status must beEqualTo(Status.InternalServerError)
-//      testRuntime.unsafeRun(stockResponse.as[String]) must contain(???)
-//    }
   }
 }
